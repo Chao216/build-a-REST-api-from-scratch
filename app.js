985bcc0a19f46f43ed69d73bbad6d2f35ff749c6
Chao216 <co.jiaan@proton.me>
2022-06-16 23:14:41 +0300

    initialized

---
+++ app.js
@@ -0,0 +1,60 @@
+//load all necessary packages.
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const app = express();
+
+//setup body-parser and static dolfer
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", (err, result) => {
+  err ? console.log(err) : console.log("succesfully connected to MongoDB")
+})
+
+//create article schema and mongoose model
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+const Article = new mongoose.model("Article", articleSchema);
+
+
+//get route
+app.get("/articles", (req, res) => {
+  Article.find({}, (err, result) => {
+    if (!err) {
+      res.send(result)
+    } else {
+      res.send(err)
+    }
+  })
+})
+//post route
+
+app.post("/articles", (req, res) => {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(err => !err ? res.send("successfully send one article") : res.send(err))
+})
+
+app.delete("/articles", (req, res) => {
+  Article.deleteMany({}, (err) => {
+    !err? res.send("succesfully deleted all articles") : res.send(err)
+  })
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req, res) => {
+  console.log("server started on port " + port)
+});
